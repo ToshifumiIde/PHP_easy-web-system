5a4b6ae40c5923f469f1f980fc3155bfba641b9e
井手 俊文 <toshifumiide198765@gmail.com>
2021-06-01 16:59:31 +0900

    connect database

--- index.php
+++ index.php
@@ -18,6 +18,22 @@
 <main>
 <h2>Practice</h2>
 <pre>
+<?php
+//例外処理。エラーが発生した際に、そのまま処理を終了させるのではなく、例外を発生させてその処理を安全に終了させる。接続がうまくいかなかった場合、例外という処理を発生させるのがtry{}catch(){}の例外処理。
+try{
+  //PDO(PHP Data Objectの略)のオブジェクトのインスタンスを生成
+  $db = new PDO(
+    //接続文字列
+    "mysql:dbname=mydb;host=localhost;charset=utf8;",//host=localhostに修正
+    //ユーザー名
+    "root",
+    //pass
+    "root"
+  );
+}catch(PDOexception $e){
+  echo "DB接続エラー" . $e->getMessage();
+}
+?>
 </pre>
 </main>
 </body>    
